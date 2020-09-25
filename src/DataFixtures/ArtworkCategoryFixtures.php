b5054e24e97f7a81f94ad7f634ea467fd404fcca
Nathan Hego <hegonathan@hotmail.fr>
2020-09-25 17:52:22 +0200

    Add DataFixtures

---
+++ src/DataFixtures/ArtworkCategoryFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ArtworkCategory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\Id\AssignedGenerator;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\Persistence\ObjectManager;
+
+class ArtworkCategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category_Artworks = [
+            [
+                'id' => 1,
+                'artwork_category_label' => 'Tableau'
+            ],
+            [
+                'id' => 2,
+                'artwork_category_label' => 'Sculpture'
+            ],
+            [
+                'id' => 3,
+                'artwork_category_label' => 'Mobilier'
+            ],
+        ];
+
+        foreach ($category_Artworks as $categoryData) {
+            $category = new ArtworkCategory();
+            $category->setId($categoryData['id']);
+            $category->setArtworkCategoryLabel($categoryData['artwork_category_label']);
+
+            $manager->persist($category);
+
+            $metaData = $manager->getClassMetadata(get_class($category));
+            $metaData->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+            $metaData->setIdGenerator(new AssignedGenerator());
+
+            $this->setReference('Category_' . $categoryData['artwork_category_label'], $category);
+        }
+
+        $manager->flush();
+    }
+}
