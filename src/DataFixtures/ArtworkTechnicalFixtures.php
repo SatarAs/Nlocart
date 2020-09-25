8ad9b875e19f7f071626c4f9540f6838b3f82e39
Nathan Hego <hegonathan@hotmail.fr>
2020-09-25 17:52:22 +0200

    Add DataFixtures

---
+++ src/DataFixtures/ArtworkTechnicalFixtures.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ArtworkTechnical;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\Id\AssignedGenerator;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\Persistence\ObjectManager;
+
+class ArtworkTechnicalFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $technical_Artworks = [
+            [
+                'id' => 1,
+                'artwork_technical_label' => 'Aérosol'
+            ],
+            [
+                'id' => 2,
+                'artwork_technical_label' => 'Encre'
+            ],
+            [
+                'id' => 3,
+                'artwork_technical_label' => 'Acrylique'
+            ],
+            [
+                'id' => 4,
+                'artwork_technical_label' => 'Bitume'
+            ],
+            [
+                'id' => 5,
+                'artwork_technical_label' => 'Canevas'
+            ],
+            [
+                'id' => 6,
+                'artwork_technical_label' => 'Collage'
+            ],
+            [
+                'id' => 7,
+                'artwork_technical_label' => 'Crayon'
+            ],
+            [
+                'id' => 8,
+                'artwork_technical_label' => 'Impression'
+            ]
+        ];
+
+        foreach ($technical_Artworks as $technicalData) {
+            $technical = new ArtworkTechnical();
+            $technical->setId($technicalData['id']);
+            $technical->setArtworkTechnicalLabel($technicalData['artwork_technical_label']);
+
+            $manager->persist($technical);
+
+            $metaData = $manager->getClassMetadata(get_class($technical));
+            $metaData->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+            $metaData->setIdGenerator(new AssignedGenerator());
+
+            $this->setReference('Technical_' . $technicalData['artwork_technical_label'], $technical);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/CustomerCategoryFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\CustomerCategory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\Id\AssignedGenerator;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\Persistence\ObjectManager;
+
+class CustomerCategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $customer_Category = [
+            [
+                'id' => 1,
+                'customer_category_label' => 'Particulier'
+            ],
+            [
+                'id' => 2,
+                'customer_category_label' => 'Professionnel'
+            ]
+        ];
+
+        foreach ($customer_Category as $customerCatData) {
+            $customCat = new CustomerCategory();
+            $customCat->setId($customerCatData['id']);
+            $customCat->setCustomerCategoryLabel($customerCatData['customer_category_label']);
+
+            $manager->persist($customCat);
+
+            $metaData = $manager->getClassMetadata(get_class($customCat));
+            $metaData->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+            $metaData->setIdGenerator(new AssignedGenerator());
+
+            $this->setReference('CustomerCategory_' . $customerCatData['customer_category_label'], $customCat);
+        }
+
+        $manager->flush();
+    }
+}
