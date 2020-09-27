ae1836864c8a5f7b51350011fdd9e1f40f589b9b
Nathan Hego <hegonathan@hotmail.fr>
2020-09-28 00:27:30 +0200

    Adding the catalog & Paginator

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Artwork;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
@@ -12,11 +16,24 @@ class CatalogController extends AbstractController
      *     "fr" : "/catalogue",
      *     "en" : "/catalog"
      * }, name="app_catalog", methods={"GET"})
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
      */
-    public function catalog()
+    public function catalog(Request $request, PaginatorInterface $paginator) : Response
     {
+        $repo = $this->getDoctrine()
+            ->getRepository(Artwork::class)
+            ->findBy([], ['artworkCreationDate' => 'desc']);
+
+        $artworks = $paginator->paginate(
+            $repo,
+            $request->query->getInt('page', 1), 18
+        );
+
         return $this->render('catalog/catalog.html.twig', [
             'route_name' => 'app_catalog',
+            'artworks' => $artworks
         ]);
     }
 }
