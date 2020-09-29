5adb46335a042f7cbc5d6c4d6117a2d62ac8ac42
SatarAs09 <satar09@hotmail.fr>
2020-09-29 17:58:33 +0200

    Show Artwork by slug

--- src/Controller/ArtworkController.php
+++ src/Controller/ArtworkController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Artwork;
+use App\Entity\TechArtwork;
+use App\Repository\ArtworkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtworkController extends AbstractController
 {
     /**
-     * @Route("/artwork", name="artwork")
+     * @Route({
+     *     "fr" : "oeuvre/voir/{slug}",
+     *     "en" : "artwork/show/{slug}"
+     * }, name="app_artwork", methods={"GET"})
+     * @param $slug
+     * @param $art
+     * @param ArtworkRepository $artworkRepository
+     * @return Response
      */
-    public function index()
+    public function show($slug, Artwork $art, ArtworkRepository $artworkRepository) : Response
     {
-        return $this->render('artwork/index.html.twig', [
-            'controller_name' => 'ArtworkController',
+        $artwork = $artworkRepository->findOneBySlug($slug);
+
+        $technical = $this->getDoctrine()->getRepository(TechArtwork::class)
+            ->findTechByArtwork($art->getId());
+
+        $random = $this->getDoctrine()
+            ->getRepository(Artwork::class)
+            ->RandomArtworks(6);
+
+
+        return $this->render('artwork/show.html.twig', [
+            'route_name' => 'app_artwork',
+            'artwork' => $artwork,
+            'technical' => $technical,
+            'random' => $random
         ]);
     }
 }
