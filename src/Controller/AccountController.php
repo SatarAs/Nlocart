68b65ff9bdda9da0979543a810b088bc4e6c73ba
SatarAs09 <satar09@hotmail.fr>
2020-10-13 01:01:48 +0200

    Add Account Page & functionality

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\Ordered;
+use App\Repository\ArtworkRepository;
+use App\Repository\CustomerRepository;
+use App\Repository\OrderedRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="account")
+     * @var ArtworkRepository
      */
-    public function index()
+    private ArtworkRepository $artworkRepository;
+    /**
+     * @var OrderedRepository
+     */
+    private OrderedRepository $orderedRepository;
+    /**
+     * @var CustomerRepository
+     */
+    private CustomerRepository $customerRepository;
+
+    /**
+     * AccountController constructor.
+     * @param ArtworkRepository $artworkRepository
+     * @param OrderedRepository $orderedRepository
+     * @param CustomerRepository $customerRepository
+     */
+    public function __construct(
+        ArtworkRepository $artworkRepository, OrderedRepository $orderedRepository,
+         CustomerRepository $customerRepository)
     {
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+        $this->artworkRepository  = $artworkRepository;
+        $this->orderedRepository  = $orderedRepository;
+        $this->customerRepository = $customerRepository;
+    }
+
+    /**
+     * @Route({
+     *     "fr" : "/mon-compte",
+     *     "en" : "/account"
+     * }, name="app_account", methods={"GET"})
+     * @return Response
+     * @throws Exception
+     */
+    public function account() : Response
+    {
+        $customer = $this->getUser();
+        $orders = $customer->getOrders();
+        $customerWaitingRes = $customerProcessedRes = $rentalArtworks = $selledArtworks = [];
+        $nbrWaitRes = $nbrValidRes = $nbrRefuseRes = $nbrRentalRes = 0;
+        /** @var Ordered $order */
+        foreach ($orders as $order){
+            if ($order->getOrderStatus() == 0){
+                array_push($customerWaitingRes, $order);
+                $nbrWaitRes++;
+            }else{
+                array_push($customerProcessedRes, $order);
+            }
+            if ($order->getOrderStatus() == 1 && $order->getDecision() == 1 ) {
+                $nbrValidRes++;
+            }elseif($order->getOrderStatus() == 1 && $order->getDecision() == 0 ){
+                $nbrRefuseRes++;
+            }
+            # Oeuvres en location
+            if ($order->getOrderRental() == 1 && $order->getOrderStatus() == 1 && $order->getOrderType() == 0){
+                $endResDate = $order->getOrderReservationEndDate();
+                $now = new \DateTime();
+                if ($endResDate > $now){
+                    array_push($rentalArtworks, $order);
+                    $nbrRentalRes ++;
+                }
+            }
+            if ($order->getOrderStatus() == 1 && $order->getOrderType() == 1 and $order->getDecision() == 0 ) array_push($selledArtworks, $order);
+        }
+
+        return $this->render('account/account.html.twig', [
+            'customer' => $customer,
+            'customerWaitingOrders' => $customerWaitingRes,
+            'customerProcessedRes' => $customerProcessedRes,
+            'customerRentalArtworks' => $rentalArtworks,
+            'customerSelledArtworks' => $selledArtworks,
+            'nbrWaitRes' => $nbrWaitRes,
+            'nbrValidRes' => $nbrValidRes,
+            'nbrRefuseRes' => $nbrRefuseRes,
+            'nbrRentalRes' => $nbrRentalRes,
         ]);
     }
 }
